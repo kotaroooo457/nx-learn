6ae673b614b6b824c5e9f203232671ef8b9acb00
kotaro-lab457 <k.chanmpmp@icloud.com>
2022-08-12 10:20:04 +0900

    generate storybook #2

---
+++ apps/core/src/app/App.stories.tsx
@@ -0,0 +1,13 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import { App } from './App';
+
+export default {
+  component: App,
+  title: 'App',
+} as ComponentMeta<typeof App>;
+
+const Template: ComponentStory<typeof App> = (args) => <App {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
--- apps/core/src/app/App.tsx
+++ apps/core/src/app/App.tsx
@@ -3,14 +3,12 @@ import { Route, Routes } from 'react-router-dom';
 
 import Table from '../pages/table';
 
-import NxWelcome from './nx-welcome';
 export function App() {
   return (
     <>
       <div className="bg-red-200">hogehoge</div>
       <Routes>
         <Route path="/table" element={<Table />} />
-        <Route path="/welcome" element={<NxWelcome title="core" />} />
       </Routes>
     </>
   );
