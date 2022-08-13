3e61b0bba611debb820844b3cd7db63160f22497
kotaro-lab457 <k.chanmpmp@icloud.com>
2022-08-14 00:40:24 +0900

    add library components

--- apps/core/src/app/App.tsx
+++ apps/core/src/app/App.tsx
@@ -1,16 +1,12 @@
-import './App.css';
 import { Route, Routes } from 'react-router-dom';
 
 import Table from '../pages/table';
 
 export function App() {
   return (
-    <>
-      <div className="bg-red-200">hogehoge</div>
-      <Routes>
-        <Route path="/table" element={<Table />} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Table />} />
+    </Routes>
   );
 }
 
